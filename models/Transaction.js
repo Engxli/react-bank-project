65a64cab28908de47a07f8bea3699093e13f01bb
Engxli <baaa555@outlook.com>
2024-01-11 18:29:36 +0300

    dine

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const TransactionSchema = new Schema({
-  type: {
-    type: String,
-    enum: ["transfer", "deposit", "withdraw"],
+const TransactionSchema = new Schema(
+  {
+    type: {
+      type: String,
+      enum: ["transfer", "deposit", "withdraw"],
+    },
+    amount: Number,
+    from: { type: Schema.Types.ObjectId, ref: "User" },
+    to: { type: Schema.Types.ObjectId, ref: "User" },
   },
-  amount: Number,
-  from: { type: Schema.Types.ObjectId, ref: "User" },
-  to: { type: Schema.Types.ObjectId, ref: "User" },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Transaction", TransactionSchema);
